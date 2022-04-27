8fdb68d6ff380c30f7c46cb8546fd23fba17fc57
gaojinshan83@gmail.com <Gaojinshan&83>
2022-04-27 15:38:03 +0800

    详情页

--- js/common.js
+++ js/common.js
@@ -88,113 +88,60 @@ jQuery(document).ready(function ($) {
     })
 
 
-
-    if ($('.hotNewsSwiper').length) {
-        var hotNewsSwiper = new Swiper('.hotNewsSwiper', {
+    if ($('.srkGoodsImgsSwiper').length) {
+        var srkGoodsImgsSwiper = new Swiper(".srkGoodsImgsSwiper", {
+            spaceBetween: 10,
+            slidesPerView: 6,
+            slideToClickedSlide: true,
+            watchSlidesVisibility: true,
+            /*避免出现bug*/
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false
+            },
+            thumbs: {
+                swiper: srkProductBannersSwiper,
+            },
+        });
+    }
+    if ($('.srkProductBannersSwiper').length) {
+        var srkProductBannersSwiper = new Swiper('.srkProductBannersSwiper', {
             effect: 'fade', // cards
             loop: true,
             autoplay: {
                 delay: 3000,
                 disableOnInteraction: false
             },
-            pagination: {
-                el: '.swiper-pagination',
-                type: 'bullets',
-                clickable: true,
-            },
-            navigation: {
-                nextEl: '.swiperButtonNext',
-                prevEl: '.swiperButtonPrev',
+            thumbs: {
+                swiper: srkGoodsImgsSwiper,
             },
         })
-
-
     }
 
+    function changeStop() {
+        srkProductBannersSwiper.autoplay.stop();
+        srkGoodsImgsSwiper.autoplay.stop();
+    }
 
+    function changeStart() {
+        srkProductBannersSwiper.autoplay.start();
+        srkGoodsImgsSwiper.autoplay.start();
+    }
+    srkProductBannersSwiper.el.onmouseover = function () {
+        changeStop()
+    }
+    srkProductBannersSwiper.el.onmouseleave = function () {
+        changeStart()
+    }
+    srkGoodsImgsSwiper.el.onmouseover = function () {
+        changeStop()
+    }
+    srkGoodsImgsSwiper.el.onmouseleave = function () {
+        changeStart()
+    }
     // $(window).resize(function () {
 
     // });
 
 
-    var preTop = 0;
-    var currTop = 0;
-    if ($('.floatingNavGlobal').length) {
-        var mlNav = $('.fnTop'); // navDIV
-        $(window).scroll(function () {
-            currTop = $(window).scrollTop();
-            if ($(window).scrollTop() < 400) {
-                mlNav.fadeOut(200);
-
-            } else if ($(window).scrollTop() > 400) {
-                mlNav.fadeIn(500);
-            }
-            preTop = $(window).scrollTop();
-        });
-    }
-
-    function switchTab(option) {
-        this.oTab_btn = this.getDom(option.tabBtn);
-        this.oTab_clist = this.getDom(option.tabCon);
-        if (!this.oTab_btn || !this.oTab_clist) return;
-        this.sCur = option.cur;
-        this.type = option.type || 'click';
-        this.nLen = this.oTab_btn.length;
-        this.int();
-    }
-    switchTab.prototype = {
-        getId: function (id) {
-            return document.getElementById(id);
-        },
-        getByClassName: function (className, parent) {
-            var elem = [],
-                node = parent != undefined && parent.nodeType == 1 ? parent.getElementsByTagName('*') : document.getElementsByTagName('*'),
-                p = new RegExp("(^|\\s)" + className + "(\\s|$)");
-            for (var n = 0, i = node.length; n < i; n++) {
-                if (p.test(node[n].className)) {
-                    elem.push(node[n]);
-                }
-            }
-            return elem;
-        },
-        getDom: function (s) {
-            var nodeName = s.split(' '),
-                p = this.getId(nodeName[0].slice(1)),
-                c = this.getByClassName(nodeName[1].slice(1), p);
-            if (!p || c.length == 0) return null;
-            return c;
-        },
-        change: function () {
-            var cur = new RegExp(this.sCur, 'g');
-            for (var n = 0; n < this.nLen; n++) {
-                this.oTab_clist[n].style.display = 'none';
-                this.oTab_clist[n].className = 'boxes tabcon'
-                this.oTab_btn[n].className = this.oTab_btn[n].className.replace(cur, '');
-            }
-        },
-        int: function () {
-            var that = this;
-            this.oTab_btn[0].className += ' ' + this.sCur;
-            this.oTab_clist[0].style.display = 'block';
-
-            for (var n = 0; n < this.nLen; n++) {
-                this.oTab_btn[n].index = n;
-                this.oTab_btn[n]['on' + this.type] = function () {
-                    that.change();
-                    that.oTab_btn[this.index].className += ' ' + that.sCur;
-                    that.oTab_clist[this.index].style.display = 'block';
-                    setTimeout(() => {
-                        that.oTab_clist[this.index].className = 'boxes tabcon show'
-                    }, 100)
-
-                }
-            }
-        }
-    }
-
-    // new switchTab({
-    //     'tabBtn': '#cdTabsBox .tabnav',
-    //     'tabCon': '#cdTabsBox .tabcon',
-    //     'cur': 'active'
-    // });
 })
